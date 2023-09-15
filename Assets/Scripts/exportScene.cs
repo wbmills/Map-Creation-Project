4516a6a8b7034d1d47c96c77775e31ccbf12153d
willmills345 <will.b.mills@outlook.com>
2023-09-15 10:32:17 +0100

    Terrain to GameObject using mesh generation applied to main project

--- Assets/Scripts/exportScene.cs
+++ Assets/Scripts/exportScene.cs
@@ -9,16 +9,23 @@ public class exportScene : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject sceneTemplate;
+    public Texture defaultTexture;
 
-    // from https://docs.unity3d.com/Packages/com.unity.formats.fbx@2.0/manual/devguide.html
     public void ExportMapAsFBX(GameObject parentObj)
     {
-        Terrain tOb = FindAnyObjectByType<Terrain>();
+        // get all objects attached to parent gameobject
         GameObject allObjs = GameObject.FindGameObjectWithTag("Object Parent");
+
+        // get terrain and convert to gameobject (Terrain type does not automatically convert to FBX,
+        // so you have to generate a mesh based on Terrain and create new GameObject with it
+        Terrain tOb = FindAnyObjectByType<Terrain>();
+        GameObject terrainGameObject = convertTerrain(tOb);
+        terrainGameObject.transform.SetParent(allObjs.transform);
+
+        // get default filepath for project and export Parent Gameobject (and all children) to FBX
         string filePath = Path.Combine(Application.dataPath, "Map.fbx");
-        //ModelExporter.ExportObject(filePath, allObjs);
-        ModelExporter.ExportObject(filePath, tOb);
-        Debug.Log($"Successful Save in {filePath}");
+        ModelExporter.ExportObject(filePath, allObjs);
+        Debug.Log($"Successful FBX Export in {filePath}");
     }
 
     // Add extras not already in parent object to parent, add player if chosen, export as scene if chosen
@@ -38,9 +45,88 @@ public class exportScene : MonoBehaviour
         return parentObj;
     }
 
-    public SceneAsset ExportAsScene(GameObject parentObj)
+    private GameObject convertTerrain(Terrain t)
     {
-        // create empty scene, add all assets from map, and return new scene
-        return null;
+        // create empty gameobject and assign it mesh generated from Terrain
+        GameObject newTerrain = new GameObject();
+        newTerrain.transform.position = t.GetPosition();
+        newTerrain.AddComponent<MeshFilter>().mesh = generateMeshFromTerrain(t, 100);
+        newTerrain.AddComponent<MeshRenderer>();
+        generateMaterial(newTerrain, t.terrainData.terrainLayers[0].diffuseTexture);
+
+        return newTerrain;
+    }
+
+    private void generateMaterial(GameObject terrain, Texture texture)
+    {
+        Renderer r = terrain.GetComponent<Renderer>();
+        Material m = new Material(Shader.Find("Standard"));
+        m.mainTexture = texture;
+        r.material = m;
+    }
+
+    // Method to generate mesh verticies and triangles from terrain gameobject
+    // terrain t = terrain to 'copy', int sqrtVerticies = square root of the maximum verticies you want to use
+    // (i.e. if sqrtVerticies = 10, maximum verticies will be 100)
+    private Mesh generateMeshFromTerrain(Terrain t, int sqrtVerticies)
+    {
+        int totalVerticies = sqrtVerticies * sqrtVerticies;
+        Mesh newMesh = new Mesh();
+
+        //Set verticies array with number of verticies (was 62500 - 250 * 250 size of terrain)
+        Vector3[] verticies = new Vector3[totalVerticies];
+
+        // work out distance between each vertex given max number of verticies and terrain area
+        // i.e. how many total verticies fit into a terrain surface area, then sqrt to get size of each individual 'square' 
+        float distanceBetweenVerticies = Mathf.Sqrt((t.terrainData.size.x * t.terrainData.size.z) / totalVerticies);
+
+        var index = 0;
+        for (int col = 0; col < Mathf.Sqrt(verticies.Length); col++)
+        {
+            for (int row = 0; row < Mathf.Sqrt(verticies.Length); row++)
+            {
+                verticies[index] = new Vector3(row * distanceBetweenVerticies,
+                    t.SampleHeight(new Vector3(row * distanceBetweenVerticies, 0, col * distanceBetweenVerticies)),
+                    col * distanceBetweenVerticies);
+                index++;
+            }
+        }
+
+        // setting normals and uvs
+        Vector3[] normals = new Vector3[totalVerticies];
+        Vector2[] uvs = new Vector2[totalVerticies];
+        for (index = 0; index < totalVerticies; index++)
+        {
+            normals[index] = Vector3.up;
+            uvs[index] = new Vector2(verticies[index].x, verticies[index].z);
+        }
+
+
+
+        // setting triangles based on verticies
+        int colLength = sqrtVerticies;
+        // total number of triangles needed (colLength-1 ^2 * 2) * number of verticies needed per triangle (*3)
+        // (colLength^2 * 2) * 3
+        int[] tris = new int[((colLength - 1) * (colLength - 1) * 2 * 3) + ((colLength - 2) * 2 * 3)];
+
+        int i = 0;
+        for (int triIndex = 0; triIndex < tris.Length; triIndex += 6)
+        {
+            tris[triIndex] = i;
+            tris[triIndex + 1] = i + colLength;
+            tris[triIndex + 2] = i + 1;
+
+            tris[triIndex + 3] = i + colLength;
+            tris[triIndex + 4] = i + colLength + 1;
+            tris[triIndex + 5] = i + 1;
+            i++;
+        }
+
+        // set values of new mesh to calculated values and return mesh
+        newMesh.vertices = verticies;
+        newMesh.normals = normals;
+        newMesh.uv = uvs;
+        newMesh.triangles = tris;
+        return newMesh;
     }
 }
--- Assets/Scripts/terrainToGameobject.cs
+++ Assets/Scripts/terrainToGameobject.cs
@@ -4,44 +4,64 @@ using UnityEngine;
 
 public class terrainToGameobject : MonoBehaviour
 {
-    public Terrain t;
-    public GameObject empty;
-    public MeshFilter meshFilter;
+    public Texture defaultTexture;
 
-    void Start()
+    private GameObject convertTerrain(Terrain t)
     {
-        // get mesh filter of empty object with no mesh
-        meshFilter = empty.GetComponent<MeshFilter>();
+        GameObject newTerrain = new GameObject();
+        newTerrain.AddComponent<MeshFilter>().mesh = generateMeshFromTerrain(t, 100);
+        newTerrain.AddComponent<MeshRenderer>();
+        setMaterialAndTexture(newTerrain);
 
-        // set mesh filter values to scripted mesh values
-/*        Mesh newMesh = new Mesh();
-        Vector3[] verticies = drawVerticies(100, 1000);
-        newMesh.vertices = verticies;
-        newMesh.triangles = drawTriangles(verticies);
-        meshFilter.mesh = newMesh;*/
-
-
-        // stuff for trialling
-        meshFilter.mesh = meshGenerationtrial();
+        return newTerrain;
     }
 
-    private Mesh meshGenerationtrial()
+    private void setMaterialAndTexture(GameObject terrain)
     {
+        Renderer r = terrain.GetComponent<Renderer>();
+        Material m = new Material(Shader.Find("Standard"));
+        m.mainTexture = defaultTexture;
+        r.material = m;
+    }
+
+    // Method to generate mesh verticies and triangles from terrain gameobject
+    private Mesh generateMeshFromTerrain(Terrain t, int sqrtVerticies)
+    {
+        int totalVerticies = sqrtVerticies * sqrtVerticies;
         Mesh newMesh = new Mesh();
 
-        Vector3[] verticies = new Vector3[62500];
+        //Set verticies array with number of verticies (was 62500 - 250 * 250 size of terrain)
+        Vector3[] verticies = new Vector3[totalVerticies];
 
-        int index = 0;
+        // work out distance between each vertex given max number of verticies and terrain area
+        // i.e. how many total verticies fit into a terrain surface area, then sqrt to get size of each individual 'square' 
+        float distanceBetweenVerticies = Mathf.Sqrt((t.terrainData.size.x * t.terrainData.size.z) / totalVerticies);
+
+        var index = 0;
         for (int col = 0; col < Mathf.Sqrt(verticies.Length); col++)
         {
             for (int row = 0; row < Mathf.Sqrt(verticies.Length); row++)
             {
-                verticies[index] = new Vector3(row, t.SampleHeight(new Vector3(row, 0, col)), col);
+                verticies[index] = new Vector3(row * distanceBetweenVerticies,
+                    t.SampleHeight(new Vector3(row * distanceBetweenVerticies, 0, col * distanceBetweenVerticies)),
+                    col * distanceBetweenVerticies);
                 index++;
             }
         }
 
-        int colLength = (int)Mathf.Sqrt(verticies.Length);
+        // setting normals and uvs
+        Vector3[] normals = new Vector3[totalVerticies];
+        Vector2[] uvs = new Vector2[totalVerticies];
+        for (index = 0; index < totalVerticies; index++)
+        {
+            normals[index] = Vector3.up;
+            uvs[index] = new Vector2(verticies[index].x, verticies[index].z);
+        }
+
+
+
+        // setting triangles based on verticies
+        int colLength = sqrtVerticies;
         // total number of triangles needed (colLength-1 ^2 * 2) * number of verticies needed per triangle (*3)
         // (colLength^2 * 2) * 3
         int[] tris = new int[((colLength - 1) * (colLength - 1) * 2 * 3) + ((colLength - 2) * 2 * 3)];
@@ -58,100 +78,12 @@ public class terrainToGameobject : MonoBehaviour
             tris[triIndex + 5] = i + 1;
             i++;
         }
+
+        // set values of new mesh to calculated values and return mesh
         newMesh.vertices = verticies;
+        newMesh.normals = normals;
+        newMesh.uv = uvs;
         newMesh.triangles = tris;
-        foreach(int x in tris)
-        {
-            print($"{x} {verticies[x]}");
-        }
         return newMesh;
     }
-
-    private Mesh tutorialMeshGeneration()
-    {
-        Mesh terrainMesh = new Mesh();
-        Vector3[] vertices = new Vector3[4];
-
-        int width = 10;
-        int height = 20;
-
-        vertices[0] = new Vector3(0, 0, 0);
-        vertices[1] = new Vector3(width, 0, 0);
-        vertices[2] = new Vector3(0, height, 0);
-        vertices[3] = new Vector3(width, height, 0);
-
-        int[] triangles = new int[6] {0, 2, 1, 2, 3, 1 };
-
-        terrainMesh.vertices = vertices;
-        terrainMesh.triangles = triangles;
-
-        return terrainMesh;
-    }
-
-    private int getMultipleOfThree(float num)
-    {
-        num = Mathf.RoundToInt(num);
-        int attempts = 0;
-        while(num % 3 != 0 && attempts < 20)
-        {
-            num++;
-            attempts++;
-        }
-        return (int)num;
-    }
-
-    private Vector3[] drawVerticies(int totalVerticies, int meshArea)
-    {
-        int sqrtVerticies = Mathf.RoundToInt(Mathf.Sqrt(totalVerticies));
-        Vector3[] verticies = new Vector3[sqrtVerticies * sqrtVerticies];
-        float distance = Mathf.Sqrt(meshArea / verticies.Length);
-        int index = 0;
-        for (float col = 0; col < sqrtVerticies; col += 1)
-        {
-            for (float row = 0; row < sqrtVerticies; row += 1)
-            {
-                float x = row * distance;
-                float z = col * distance;
-                //float y = t.SampleHeight(new Vector3(x, 10, z));
-                float y = 3;
-                verticies[index] = new Vector3(x, y, z);
-                index++;
-            }
-        }
-        return verticies;
-    }
-
-    private int[] drawTriangles(Vector3[] verticies)
-    {
-        int[] triangles = new int[verticies.Length];
-        int colLength = (int)Mathf.Sqrt(verticies.Length);
-
-        for (int i = 0; i < verticies.Length / 4; i++)
-        {
-            triangles[i] = i;
-            triangles[i + 1] = i + colLength;
-            triangles[i + 2] = i + 1;
-
-            triangles[i + 3] = i + colLength;
-            triangles[i + 4] = i + colLength + 1;
-            triangles[i + 5] = i + 1;
-        }
-        print(triangles[1]);
-        return triangles;
-    }
-
-/*    private void OnDrawGizmos()
-    {
-        try
-        {
-            foreach (Vector3 point in meshFilter.mesh.vertices)
-            {
-                Gizmos.DrawSphere(point, .3f);
-            }
-        }
-        catch (System.Exception)
-        {
-
-        }
-    }*/
 }

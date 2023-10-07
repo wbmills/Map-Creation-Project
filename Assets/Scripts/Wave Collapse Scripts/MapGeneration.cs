d0a3cef76071fc25a830cc945b279d2c9024c463
willmills345 <will.b.mills@outlook.com>
2023-10-07 15:10:30 +0100

    Terrain is painted with road, wall bugs fixed

--- Assets/Scripts/Wave Collapse Scripts/MapGeneration.cs
+++ Assets/Scripts/Wave Collapse Scripts/MapGeneration.cs
@@ -17,26 +17,61 @@ public class Road
 
     public Vector3 pointA;
     public Vector3 pointB;
+    public Vector3 direction;
+    public Road prev;
     public Road pointer;
 
     // width and length of road, where 
     public float width;
     public float length;
+
+    public List<Vector3> road = new List<Vector3>();
 }
+
+public class MapConfig
+{
+    public Terrain terrain;
+    public float density;
+    public float uniformity;
+    public string theme;
+    public float maxRoadLength;
+    public float maxRoadWidth;
+    public float minRoadLength;
+    public float minRoadWidth;
+    public GameObject[] obsOfTheme;
+    public Vector3 initPoint;
+}
+
 public class MapGeneration : MonoBehaviour
 {
     private Terrain mapTerrain;
     private Vector3 terrainCentre;
     private List<GameObject> allObjectsInMap;
     private List<Road> allRoads;
+    private GameObject[] obsOfTheme;
+    private MapConfig curConfig;
 
     void Start()
     {
         SetMapTerrain();
+        SetDefaults();
+        obsOfTheme = GetObjectsOfTheme("default");
+        curConfig = SetCurrentMapConfig();
+        GenerateMap();
+    }
+
+    private void ResetTerrainPaint()
+    {
+        foreach(Road r in allRoads)
+        {
+            PaintTerrain(r.road, true);
+        }
+    }
+
+    private void SetDefaults()
+    {
         allRoads = new List<Road>();
         allObjectsInMap = new List<GameObject>();
-        
-        GenerateMap();
     }
 
     void Update()
@@ -49,66 +84,170 @@ public class MapGeneration : MonoBehaviour
         float minRoadWidth, float maxRoadLength, float minRoadLength, Vector3 centre = default, float randomness = 0f, float angleResolution = 1f, string theme = "Default")*/
     private void GenerateMap()
     {
-        string theme = "default";
-        Vector3 init_point = new Vector3(50f, 2.5f, 20);
-        float maxRoadLength = 60f;
-        float maxRoadWidth = 20f;
-        GameObject[] obsOfTheme = Resources.LoadAll<GameObject>($"Map Generation/{theme}");
+        string theme = curConfig.theme;
+        float maxRoadLength = curConfig.maxRoadLength;
+        float maxRoadWidth = curConfig.maxRoadWidth;
+        float minRoadWidth = curConfig.minRoadWidth;
+        float minRoadLength = curConfig.minRoadLength;
+        GameObject[] allObjects = GetObjectsOfTheme(theme);
         List<Vector3> directionOptions = new List<Vector3>() { Vector3.forward, Vector3.right, Vector3.left };
-        if (!CheckCompletePrefabSet(theme, obsOfTheme))
-        {
-            throw new Exception("Not all the required prefabs are present");
-        }
 
         int i = 0;
         bool end = false;
-        float curPos = init_point.z;
         float maxRoads = 20;
+        Vector3 newDir;
         while (!end && allRoads.Count < maxRoads && i < 100)
         {
+            newDir = directionOptions[Random.Range(0, directionOptions.Count)];
+            float newWidth = Random.Range(minRoadWidth, maxRoadWidth);
+            float newLength = Random.Range(minRoadLength, maxRoadLength);
 
-            Road tempRoad = new Road();
-            Vector3 newDir = directionOptions[Random.Range(0, directionOptions.Count)];
-            Vector3 newPointA;
-            if (allRoads.Count == 0)
-            {
-                newPointA = init_point;
-            }
-            else
-            {
-                Road prevRoad = allRoads[allRoads.Count - 1];
-                Vector3 prevRoadDir = (prevRoad.pointB - prevRoad.pointA).normalized;
-                if (newDir != prevRoadDir)
-                {
-                    newPointA = prevRoad.pointB + (prevRoadDir * (maxRoadWidth / 2)) + (newDir * (prevRoad.width / 2));
-                }
-                else
-                {
-                    newPointA = allRoads[allRoads.Count - 1].pointB;
-                }
-                allRoads[allRoads.Count - 1].pointer = tempRoad;
-            }
-
-            tempRoad.pointA = newPointA;
-            tempRoad.centre = tempRoad.pointA + newDir * (maxRoadLength / 2);
-            tempRoad.pointB = tempRoad.centre + newDir * (maxRoadLength / 2);
-            tempRoad.length = maxRoadLength;
-            tempRoad.width = maxRoadWidth;
-            tempRoad.pointer = null;
-            if (checkInTerrain(tempRoad))
-            {
-                SetRoadObjects(tempRoad, .7f, .9f, "Building", obsOfTheme);
-                allRoads.Add(tempRoad);
-            }
-            else
-            {
-
-            }
+            GenerateRoad(20, 60, newDir);
             i++;
         }
         print($"Road Count: {allRoads.Count}");
     }
 
+
+    private List<Vector3> SetRoadVectors(Road road)
+    {
+        List<Vector3> positionsToPaint = new List<Vector3>();
+        
+        int tries = 0;
+        if (road.prev != null)
+        {
+            Vector3 prevRoadWithWidth = road.prev.pointB + (road.prev.direction * (road.width / 2));
+            positionsToPaint.Add(prevRoadWithWidth);
+        }
+        else
+        {
+            positionsToPaint.Add(road.pointA);
+        }
+
+        while (tries < 200 && (road.pointB - positionsToPaint[positionsToPaint.Count - 1]).normalized == road.direction)
+        {
+            positionsToPaint.Add(positionsToPaint[positionsToPaint.Count - 1] + (road.direction * 2));
+        }
+        positionsToPaint.Add(positionsToPaint[positionsToPaint.Count - 1] + (road.direction * 2));
+
+        return positionsToPaint;
+    }
+
+    private void OnApplicationQuit()
+    {
+        ResetTerrainPaint();
+    }
+
+    private GameObject[] GetObjectsOfTheme(string theme)
+    {
+        GameObject[] obsOfTheme = Resources.LoadAll<GameObject>($"Map Generation/{theme}");
+        if (!CheckCompletePrefabSet(theme, obsOfTheme))
+        {
+            throw new Exception("Not all the required prefabs are present");
+        }
+        return obsOfTheme;
+    }
+
+    private MapConfig SetCurrentMapConfig()
+    {
+        MapConfig tempConfig = new MapConfig();
+        tempConfig.minRoadWidth = 20f;
+        tempConfig.maxRoadWidth = 60f;
+        tempConfig.minRoadLength = 40f;
+        tempConfig.maxRoadLength = 80f;
+        tempConfig.theme = "default";
+        tempConfig.initPoint = new Vector3(60f, 0f, 60f);
+        return tempConfig;
+    }
+
+    public void PaintTerrain(List<Vector3> positionsToPaint, bool reset=false)
+    {
+        Terrain terrain = mapTerrain;
+        foreach (Vector3 tile in positionsToPaint)
+        {
+            Vector3 terrainPos = tile - terrain.transform.position;
+            Vector3 mapPos = new Vector3(terrainPos.x / terrain.terrainData.size.x, 0, terrainPos.z / terrain.terrainData.size.z);
+            float xCoord = mapPos.x * terrain.terrainData.alphamapWidth;
+            float zCoord = mapPos.z * terrain.terrainData.alphamapHeight;
+            int posX = (int)xCoord;
+            int posZ = (int)zCoord;
+            if (posX > 0 && posX < terrain.terrainData.alphamapWidth && posZ > 0 && posZ < terrain.terrainData.alphamapHeight)
+            {
+                int c = 7;
+                float[,,] splatmapData = terrain.terrainData.GetAlphamaps(posX, posZ, c, c);
+                for (int y = 0; y < c; y++)
+                {
+                    for (int x = 0; x < c; x++)
+                    {
+                        if (reset)
+                        {
+                            splatmapData[x, y, 0] = 1;
+                            splatmapData[x, y, 1] = 0;
+                        }
+                        else
+                        {
+                            splatmapData[x, y, 0] = 0;
+                            splatmapData[x, y, 1] = 1;
+                        }
+                    }
+                }
+                terrain.terrainData.SetAlphamaps(posX, posZ, splatmapData);
+            }
+        }
+    }
+
+    private Road GenerateRoad(float maxRoadWidth, float maxRoadLength, Vector3 relativeDirection, bool blank=false)
+    {
+        Road tempRoad = new Road();
+        Vector3 newDir = relativeDirection;
+        Vector3 newPointA;
+        Road prevRoad = null;
+        if (allRoads.Count == 0)
+        {
+            newPointA = curConfig.initPoint;
+        }
+        else
+        {
+            prevRoad = allRoads[allRoads.Count - 1];
+            Vector3 prevRoadDir = (prevRoad.pointB - prevRoad.pointA).normalized;
+            if (newDir != prevRoadDir)
+            {
+                newPointA = prevRoad.pointB + (prevRoadDir * (maxRoadWidth / 2)) + (newDir * (prevRoad.width / 2));
+            }
+            else
+            {
+                newPointA = allRoads[allRoads.Count - 1].pointB;
+            }
+            allRoads[allRoads.Count - 1].pointer = tempRoad;
+        }
+
+        tempRoad.pointA = newPointA;
+        tempRoad.centre = tempRoad.pointA + newDir * (maxRoadLength / 2);
+        tempRoad.pointB = tempRoad.centre + newDir * (maxRoadLength / 2);
+        tempRoad.length = maxRoadLength;
+        tempRoad.width = maxRoadWidth;
+        tempRoad.direction = (tempRoad.pointB - tempRoad.pointA).normalized;
+        tempRoad.pointer = null;
+        tempRoad.prev = prevRoad;
+        tempRoad.road = SetRoadVectors(tempRoad);
+
+        if (!blank && checkInTerrain(tempRoad))
+        {
+            SetRoadObjects(tempRoad, .7f, .9f, "Building", obsOfTheme);
+            PaintTerrain(tempRoad.road);
+            allRoads.Add(tempRoad);
+        }
+        else if (blank)
+        {
+            allRoads.Add(tempRoad);
+        }
+        else
+        {
+            tempRoad = null;
+        }
+
+        return tempRoad;
+    }
 
     private bool checkInTerrain(Road road)
     {
@@ -126,6 +265,19 @@ public class MapGeneration : MonoBehaviour
             }
         }
         return true;
+    }
+
+    // still deciding how 'modular' this approach would be 
+    private GameObject[] GetGameObjectOfType(string objectType)
+    {
+        if (obsOfTheme == null)
+        {
+            return null;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     // the lower uniformity, the more equal distance objects will be from each other
@@ -193,13 +345,18 @@ public class MapGeneration : MonoBehaviour
                 //newOb.transform.Rotate(new Vector3(0, 90, 0));
                 road.buildings.Add(newOb);
                 Instantiate(newOb);
-                //BuildWall(wall, newOb, road);
+                BuildWall(wall, newOb, road);
                 i++;
             }
         }
     }
 
     private void OnDrawGizmos()
+    {
+        RoadDebug();
+    }
+
+    private void RoadDebug()
     {
         if (allRoads != null)
         {
@@ -211,8 +368,8 @@ public class MapGeneration : MonoBehaviour
 
                 // x = r(cos(degrees°)), y = r(sin(degrees°)).
                 Vector3 direction = (r.pointB - r.pointA).normalized;
-                float angleFromCentre = Vector3.Angle(Vector3.forward, direction) * (Mathf.PI/180);
-                float addX = r.width * Mathf.Sin(angleFromCentre + (90 * (Mathf.PI/180)));
+                float angleFromCentre = Vector3.Angle(Vector3.forward, direction) * (Mathf.PI / 180);
+                float addX = r.width * Mathf.Sin(angleFromCentre + (90 * (Mathf.PI / 180)));
                 float addY = r.width * Mathf.Cos(angleFromCentre + (90 * (Mathf.PI / 180)));
                 Vector3 pointALeft = new Vector3(r.pointA.x + addX, r.pointA.y, r.pointA.z + addY);
                 Vector3 pointARight = new Vector3(r.pointA.x - addX, r.pointA.y, r.pointA.z - addY);
@@ -307,12 +464,12 @@ public class MapGeneration : MonoBehaviour
         Vector3 rightBounds;
         RaycastHit check = new RaycastHit();
         Vector3 direction = (road.pointB - road.pointA).normalized;
-        Physics.Raycast(ob.transform.position, -direction, out check);
+        Physics.Raycast(ob.transform.position, -direction, out check, maxDistance:Vector3.Distance(road.pointA, road.pointB));
         if (isCollisionAChild(check.collider, ob) | (road.pointA - check.point).normalized == direction)
         {
             return;
         }
-        else if (check.collider && check.collider.gameObject != ob.gameObject)
+        else if (check.collider && check.collider.gameObject != ob.gameObject && check.collider.transform.rotation == ob.transform.rotation)
         {
             leftBounds = check.point;
         }
@@ -322,14 +479,13 @@ public class MapGeneration : MonoBehaviour
         }
 
         Physics.Raycast(leftBounds, direction, out check);
-        if (check.collider)
+        if (check.collider && check.collider.transform.rotation == ob.transform.rotation)
         {
             rightBounds = check.point;
         }
         else
         {
             return;
-            //rightBounds = road.pointB;
         }
 
         GameObject tempObj = CreateNewObject(wallObject, Vector3.zero, Quaternion.Euler(0, 0, 0), null);
